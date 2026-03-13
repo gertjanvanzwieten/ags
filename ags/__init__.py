a230ce3e00760e1eea8d5a850b19843ae8412846
Gertjan van Zwieten <gertjanvanzwieten@evalf.nl>
2026-03-13 12:01:16 +0100

    Support .yaml extension
    This patch adds support for the .yaml file extension, in addition to the
    existing .yml extension.

--- ags/__init__.py
+++ ags/__init__.py
@@ -4,7 +4,7 @@ __version__ = "0.2.2"
 def _get_backend_for(path):
     if path.endswith(".json"):
         from . import json as backend
-    elif path.endswith(".yml"):
+    elif path.endswith(".yml") or path.endswith(".yaml"):
         from . import yaml as backend
     else:
         raise ValueError(f"unrecognized file format for path {path!r}")
